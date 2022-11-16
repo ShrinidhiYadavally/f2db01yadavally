637509f1a3bad9e04efa5d82b0868bed7b41ef2f
Shrinidhi Yadavally <y.shrinidhireddy@gmail.com>
2022-11-16 17:58:31 -0600

    chnages

--- controllers/kite.js
+++ controllers/kite.js
@@ -87,3 +87,72 @@ exports.kite_view_all_Page = async function(req, res) {
         res.send(`{"error": ${err}}`); 
     }   
 }; 
+
+
+// Handle kite delete form on DELETE.
+exports.kite_delete = async function(req, res) {
+    console.log("delete "  + req.params.id) 
+    try { 
+        result = await kite.findByIdAndDelete( req.params.id) 
+        console.log("Removed " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": Error deleting ${err}}`); 
+    } 
+};
+
+// Handle a show one view with id specified by query
+exports.kite_view_one_Page = async function(req, res) { 
+    console.log("single view for id "  + req.query.id) 
+    try{ 
+        result = await kite.findById( req.query.id) 
+        res.render('kitedetail',  
+{ title: 'kite Detail', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+};
+
+// Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.kite_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('kitecreate', { title: 'kite Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+};
+
+// Handle building the view for updating a costume.
+// query provides the id
+exports.kite_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await kite.findById(req.query.id) 
+        res.render('kiteupdate', { title: 'kite Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+};
+// Handle a delete one view with id from query
+exports.kite_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await kite.findById(req.query.id) 
+        res.render('kitedelete', { title: 'kite Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+};
--- routes/kite.js
+++ routes/kite.js
@@ -4,7 +4,16 @@ var router = express.Router();
 
 
 
-/* GET kite */ 
-router.get('/', kite_controlers.kite_view_all_Page ); 
+/* GET costumes */
+router.get('/', kite_controlers.kite_view_all_Page);
+
+router.get('/detail', kite_controlers.kite_view_one_Page); 
+
+router.get('/create', kite_controlers.kite_create_Page); 
+
+router.get('/update', kite_controlers.kite_update_Page);
+
+router.get('/delete', kite_controlers.kite_delete_Page);
+
 
 module.exports = router;
