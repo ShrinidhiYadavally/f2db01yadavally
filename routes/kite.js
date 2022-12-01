c8c9fde4d1a2b51da1c6fd0d761d90cef3f6cdfc
Shrinidhi Yadavally <y.shrinidhireddy@gmail.com>
2022-11-30 20:58:21 -0600

    changes

--- routes/kite.js
+++ routes/kite.js
@@ -3,17 +3,23 @@ const kite_controlers= require('../controllers/kite');
 var router = express.Router();
 
 
-
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+    }
 /* GET costumes */
 router.get('/', kite_controlers.kite_view_all_Page);
 
 router.get('/detail', kite_controlers.kite_view_one_Page); 
 
-router.get('/create', kite_controlers.kite_create_Page); 
+router.get('/create', secured,kite_controlers.kite_create_Page); 
 
-router.get('/update', kite_controlers.kite_update_Page);
+router.get('/update', secured,kite_controlers.kite_update_Page);
 
-router.get('/delete', kite_controlers.kite_delete_Page);
+router.get('/delete', secured,kite_controlers.kite_delete_Page);
 
 
 module.exports = router;
